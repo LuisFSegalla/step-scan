abf84c3a3535c1f2e764fdd0e89d0182c9e14b55
Luis Segalla <Luis.Segalla@diamond.ac.uk>
2026-03-05 11:38:00 +0000

    Add code for simple step scan interface

---
+++ src/main.py
@@ -0,0 +1,21 @@
+"""main."""
+
+from optparse import OptionParser
+
+from qcm_stability import run_scan
+
+
+def main():
+    """Main entrypoint."""
+    parser = OptionParser()
+    parser.add_option("-f",
+                      "--filename",
+                      action="store",
+                      type="string",
+                      dest="filename")
+    (config, _) = parser.parse_args()
+    run_scan(config.filename)
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/qcm_stability.py
@@ -0,0 +1,111 @@
+"""My module for running step scans with the PandA."""
+
+import json
+import logging
+
+from cothread import Sleep
+from cothread.catools import caget, caput
+from p4p.client.thread import Context
+
+ctxt = Context("pva")
+
+
+def run_scan(configpath: str = ""):
+    """Method for running the step scan.
+
+    This will open a configuration file provided by the user
+    and use it's information to configure the scan.
+    """
+    try:
+        f = open(configpath)
+        options = json.loads(f.read())
+    except OSError as e:
+        logging.error(f"Could not open the file!\n{e}")
+    """Method for running a step scan."""
+    #############################################
+    # USER CONFIG
+    motorPV = options["motorPV"]  # CA
+    pandaPV = options["pandaPV"]  # PVA
+
+    # Motor config
+    startPos = options["startPos"]
+    stopPos = options["stopPos"]
+    step = options["step"]
+    mres = options["mres"]
+
+    # Panda config
+    filepath = options["filepath"]
+    filename = options["filename"]
+    triggersPerStep = options["triggersPerStep"]
+    #############################################
+
+    motorRBV = motorPV + ".RBV"
+    motorDMOV = motorPV + ".DMOV"
+    motorTWV = motorPV + ".TWV"
+    motorTWF = motorPV + ".TWF"
+
+    # PCOMP block
+    pandaPcompEnable = pandaPV + ":PCOMP1:ENABLE"
+    pandaPcompStart = pandaPV + ":PCOMP1:START"
+    pandaPcompStep = pandaPV + ":PCOMP1:STEP"
+    pandaPcompPulses = pandaPV + ":PCOMP1:PULSES"
+
+    # Pulse Block
+    pandaPulseWidth = pandaPV + "PULSE1:WIDTH"
+    pandaPulsePulses = pandaPV + ":PULSE1:PULSES"
+
+    # PCAP Block
+    pandaPCAPEnable = pandaPV + ":PCAP:ENABLE"
+    pandaPcapArm = pandaPV + ":PCAP:ARM"
+    # Data Blocks
+    pandaDataDirectory = pandaPV + ":DATA:HDF_DIRECTORY"
+    pandaDataFilename = pandaPV + ":DATA:HDF_FILE_NAME"
+    pandaDataCapture = pandaPV + ":DATA:CAPTURE"
+
+    numSteps = int(abs(stopPos - startPos) / step)
+    caput(motorTWV, step)
+
+    # Move motor to start position caput
+    caput(motorPV, startPos - step, wait=True, timeout=10)
+    dmov = caget(motorDMOV)
+
+    while not motorDMOV:
+        Sleep(1)
+        dmov = caget(motorDMOV)
+
+    print(f"Motor reached initial position ({startPos})")
+
+    # Configure the PandA to receive data
+    ctxt.put(pandaDataDirectory, filepath)
+    ctxt.put(pandaDataFilename, filename)
+
+    # Configure PCOMP
+    ctxt.put(pandaPcompStart, int(startPos / mres))
+    ctxt.put(pandaPcompStep, int(abs(step / mres)))
+    ctxt.put(pandaPcompPulses, numSteps)
+
+    # Configure Pulse
+    ctxt.put(pandaPulsePulses, triggersPerStep)
+
+    ctxt.put(pandaPcompEnable, "ONE")
+    ctxt.put(pandaPCAPEnable, "ONE")
+    ctxt.put(pandaDataCapture, 1)
+    ctxt.put(pandaPcapArm, 1)
+
+    currentPos = caget(motorRBV)
+
+    while currentPos <= stopPos:
+        caput(motorTWF, 1)
+        dmov = caget(motorDMOV)
+        while not dmov:
+            Sleep(1)
+            dmov = caget(motorDMOV)
+
+        Sleep(2)
+        currentPos = caget(motorRBV)
+
+    ctxt.put(pandaPcompEnable, "ZERO")
+    ctxt.put(pandaPCAPEnable, "ZERO")
+    ctxt.put(pandaDataCapture, 0)
+    ctxt.put(pandaPcapArm, 0)
+    print("finished the program")
